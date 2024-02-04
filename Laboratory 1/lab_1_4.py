1b92f7488620c6b7426da49636375a2e772ae25b
Dafter <dafterplay@mail.ru>
2024-02-04 12:01:58 +0300

    fix name in lab_1_4

--- Laboratory 1/lab_1_4.py
+++ Laboratory 1/lab_1_4.py
@@ -1,14 +1,14 @@
 # %%
 from thinkdsp import read_wave
 
-wave3 = read_wave('680840__seth_makes_sounds__homemade.wav')
-wave3.normalize()
-wave3.plot()
-wave3.make_audio()
+wave = read_wave('680840__seth_makes_sounds__homemade.wav')
+wave.normalize()
+wave.plot()
+wave.make_audio()
 # %%
 def stretch(wave, factor):
     wave.ts *= factor
     wave.framerate /= factor
 
-stretch(wave3, 0.5)
-wave3.make_audio()
+stretch(wave, 0.5)
+wave.make_audio()
