89dff760c1f1680e3e8134122c60ba568133d652
DafterPlay <dafterplay@mail.ru>
2024-03-05 01:03:12 +0300

    add lab_8_1

---
+++ Laboratory 8/lab_8_1.py
@@ -0,0 +1,41 @@
+# %%
+import scipy.signal
+import matplotlib.pyplot as plt
+import numpy as np
+from thinkdsp import SquareSignal, decorate, zero_pad, Wave
+# %%
+def plot_filter(M=11, std=2):
+    signal = SquareSignal(freq=440)
+    wave = signal.make_wave(duration=1, framerate=44100)
+    spectrum = wave.make_spectrum()
+
+    gaussian = scipy.signal.gaussian(M=M, std=std)
+    gaussian /= sum(gaussian)
+
+    ys = np.convolve(wave.ys, gaussian, mode='same')
+    smooth =  Wave(ys, framerate=wave.framerate)
+    spectrum2 = smooth.make_spectrum()
+
+    # plot the ratio of the original and smoothed spectrum
+    amps = spectrum.amps
+    amps2 = spectrum2.amps
+    ratio = amps2 / amps    
+    ratio[amps<560] = 0
+
+    # plot the same ratio along with the FFT of the window
+    padded =  zero_pad(gaussian, len(wave))
+    dft_gaussian = np.fft.rfft(padded)
+
+    plt.plot(np.abs(dft_gaussian), color='gray', label='Gaussian filter')
+    plt.plot(ratio, label='amplitude ratio')
+
+    decorate(xlabel='Frequency (Hz)', ylabel='Amplitude ratio')
+    plt.show()
+# %%
+from ipywidgets import interact
+import ipywidgets as widgets
+
+slider = widgets.IntSlider(min=2, max=100, value=11)
+slider2 = widgets.FloatSlider(min=0, max=20, value=2)
+interact(plot_filter, M=slider, std=slider2)
+# %%
