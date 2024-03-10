f3152ae9bd778b852aa7a05b2024f9884c7ffd07
Dafter <dafterplay@mail.ru>
2024-03-10 17:20:44 +0300

    add lab_9_2.py

---
+++ Laboratory 9/lab_9_2.py
@@ -0,0 +1,13 @@
+from thinkdsp import TriangleSignal, decorate
+# %%
+in_wave = TriangleSignal(freq=50).make_wave(duration=0.1, framerate=44100)
+in_wave.plot()
+decorate(xlabel='Time (s)')
+# %%
+out_wave = in_wave.diff()
+out_wave.plot()
+decorate(xlabel='Time (s)')
+# %%
+out_wave2 = in_wave.make_spectrum().differentiate().make_wave()
+out_wave2.plot()
+decorate(xlabel='Time (s)')
